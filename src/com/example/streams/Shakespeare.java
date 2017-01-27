7f4fda357c1437cce5dbabc1046a14c0dc3e8263
Daniel Martinez <dmartinez@itexico.net>
2017-01-27 16:44:45 -0600

    Finding shakespeare best world for scrabble

--- src/com/example/streams/Shakespeare.java
+++ src/com/example/streams/Shakespeare.java
@@ -3,7 +3,10 @@ package com.example.streams;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 
 public class Shakespeare {
@@ -20,6 +23,27 @@ public class Shakespeare {
 		
 		System.out.println("# words of Shakespeare : " + shakespeareWords.size());
 		System.out.println("# words of Scrabble : " + scrabbleWords.size());
+		
+		final int[] scrabbleENScore = {
+				//a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p,  q, r, s, t, u, v, w, x, y,  z
+				  1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
+		};
+		
+		
+		Function<String, Integer> score = 
+				word -> word.chars().map(letter -> scrabbleENScore[letter - 'a']).sum();
+				
+		ToIntFunction<String> intScore =
+				word -> word.chars().map(letter -> scrabbleENScore[letter - 'a']).sum();
+				
+		System.out.println("Score of hello: " + intScore.applyAsInt("hello"));
+		
+		String bestWord = shakespeareWords.stream()
+			.filter(word -> scrabbleWords.contains(word))
+			.max(Comparator.comparing(score))
+			.get();
+		
+		System.out.println("Best word: " + bestWord);
 		
 	}
 	
